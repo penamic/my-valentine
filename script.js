b17ad8c3fc509834e1e22c8993b974f9a0b30ae1
penamic <100451515+penamic@users.noreply.github.com>
2024-02-06 19:23:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function generateMessage(noCount) {
 }
 
 function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
+  catImg.src = `img/cat-${image}.png`;
 }
 
 function updateNoButtonText() {
